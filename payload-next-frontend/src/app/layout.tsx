5b0f52297f61bc7fecd3fa260641673d8acd6159
Gold240sx <240designworks@gmail.com>
2024-04-04 20:02:32 -0500

    Section 2: Vercel Analytics

--- payload-next-frontend/src/app/layout.tsx
+++ payload-next-frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter, Roboto_Mono } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 import Script from "next/script"
 import Head from "next/head"
 import localFont from "next/font/local"
@@ -71,6 +72,7 @@ export default function RootLayout({
 					)}
 			</Head>
 			<body className={inter.className}>{children}</body>
+			<Analytics />
 		</html>
 	)
 }
