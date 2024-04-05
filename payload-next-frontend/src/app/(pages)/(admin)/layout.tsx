91a68276493f70d3f3272ce099d6f9ef18ebcf5f
Gold240sx <240designworks@gmail.com>
2024-04-04 20:39:56 -0500

    Section 2: Deployment Fix

--- payload-next-frontend/src/app/(pages)/(admin)/layout.tsx
+++ payload-next-frontend/src/app/(pages)/(admin)/layout.tsx
@@ -1,7 +1,12 @@
 import React, { ReactNode } from "react"
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-	return <div>{children}</div>
+	return (
+		<div>
+			<h1>Dashboard Layout </h1>
+			{children}
+		</div>
+	)
 }
 
 export default DashboardLayout
