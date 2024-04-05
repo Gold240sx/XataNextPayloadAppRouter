d7d832b7ba2091d9df9d1cd23f042fb4e5814485
Gold240sx <240designworks@gmail.com>
2024-04-05 06:11:50 -0500

    Section 3.  Separation Complete

---
+++ README.md
@@ -0,0 +1,25 @@
+# Xata DB - Next JS SRC - Payload CMS <img style=" height:38px; padding-top:10px;" src="https://i.ibb.co/ydRpMMZ/Untitled.png"/>
+
+## Dev Notes
+
+repo: https://github.com/Gold240sx/XataNextPayloadAppRouter<br />
+demo: https://2024-portfolio-template.vercel.app<br />
+filePath: CODE/WebDev/2024/2024-portfolio-xata <br />
+vercel: https://vercel.com/gold240sx/2024-portfolio-template/deployments<br />
+xata Database: 2024-portfolio <br />
+
+## About
+
+This is a Next JS monorepo, _USING_ the App router. It utlizes, Xata DB, Next JS, and Payload CMS, as well as React-Email, Resend and others.
+
+## Tech Stack:
+
+-   Next JS
+-   Xata DB
+-   Payload CMS
+-   Tailwind CSS
+-   React-Email
+-   Axios
+-   Vercel
+
+### For original installation instructions see DEVreadme.md
---
+++ payload-next-frontend/src/app/layout.tsx
@@ -0,0 +1,78 @@
+import type { Metadata } from "next"
+import { Inter, Roboto_Mono } from "next/font/google"
+// import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script"
+import Head from "next/head"
+import localFont from "next/font/local"
+import "./globals.css"
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+})
+
+const roboto_mono = Roboto_Mono({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-roboto-mono",
+})
+
+// const roboto = localFont({
+// 	src: [
+// 		{
+// 			path: "./Roboto-Regular.woff2",
+// 			weight: "400",
+// 			style: "normal",
+// 		},
+// 		{
+// 			path: "./Roboto-Italic.woff2",
+// 			weight: "400",
+// 			style: "italic",
+// 		},
+// 		{
+// 			path: "./Roboto-Bold.woff2",
+// 			weight: "700",
+// 			style: "normal",
+// 		},
+// 		{
+// 			path: "./Roboto-BoldItalic.woff2",
+// 			weight: "700",
+// 			style: "italic",
+// 		},
+// 	],
+// 	display: "swap",
+// })
+
+export const metadata: Metadata = {
+	title: "Xata 2024 Portfolio",
+	description: "Michael Martella's portfolio created in 2024",
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html
+			lang="en"
+			//  "add ${roboto.className} with my local fonts"
+			className={`${inter.variable} ${roboto_mono.variable}`}>
+			<Head>
+				{process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+					process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+						<Script
+							src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+							data-website-id={
+								process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID
+							}
+							strategy="lazyOnload"
+						/>
+					)}
+			</Head>
+			<body className={inter.className}>{children}</body>
+			{/* <Analytics /> */}
+		</html>
+	)
+}
