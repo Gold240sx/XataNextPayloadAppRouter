ede02b04f4ddd6707e34f4f62745a1033f9f4232
Gold240sx <240designworks@gmail.com>
2024-04-09 20:45:38 -0500

    removed potential deployment conflict

--- payload-next-frontend/src/dependencies/1_index.ts
+++ payload-next-frontend/src/dependencies/1_index.ts
@@ -13,8 +13,6 @@ export * as TailwindMerge from "./tailwind-merge" //https://github.com/dcastil/t
 export * as Vaul from "./vaul" //https://github.com/emilkowalski/vaul
 export * as Axios from "./axios"
 //https:flowbite.com/figma/
-export * as FlowbiteReact from "./flowbite-react" //https://flowbite-react.com
-export * as Nivo from "./nivo" //https://nivo.rocks
 export * as ApexCharts from "./apex-charts" //https://apexcharts.com/docs/react-charts/
 
 // not added yet
@@ -23,14 +21,22 @@ export * as ReactSelect from "./react-select" //https://react-select.com/home
 export * as ReactHookForm from "./react-hook-form" //https://react-hook-form.com
 export * as Zod from "./zod" //https://zod.dev // cool animation!!!!
 
-// require "use client"
+//==== require "use client"
 // export * as framerMotion from "./framerMotion" // https://www.framer.com/motion/
 // export * as inputOtp from "./input-otp" // https://input-otp.rodz.dev
 // export * as reactDayPicker from "./react-day-picker" // https://react-day-picker.js.org
 // export * as reactResizablePanels from "./react-resizable-panels" //https://github.com/bvaughn/react-resizable-panels
 
-// window is not defined
+//==== window is not defined
 // export * as apexCharts from "./apex-charts" // https://apexcharts.com/docs/react-charts/
 
-// Uncaught TypeError: Super expression must either be null or a function, not undefined
+//==== Uncaught TypeError: Super expression must either be null or a function, not undefined
 // export * as Recharts from "./recharts" //https://recharts.org/en-US
+
+//====
+// react version less than react-18 erro at vercel deployment
+// Fix the upstream dependency conflict, or retry
+//====
+// npm ERR! this command with --force or --legacy-peer-deps
+// // export * as Nivo from "./nivo" //https://nivo.rocks
+// export * as FlowbiteReact from "./flowbite-react" //https://flowbite-react.com
