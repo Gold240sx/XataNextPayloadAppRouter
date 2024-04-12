0837cb8b2a549c8bcdae81d54a021b16a7b1a091
Gold240sx <240designworks@gmail.com>
2024-04-12 01:33:06 -0500

    Working Payload 3.0 Beta Admin Panel

--- payload-next-frontend/next.config.js
+++ payload-next-frontend/next.config.js
@@ -1,12 +1,13 @@
+const { withPayload } = require('@payloadcms/next/withPayload')
 // /** @type {import('next').NextConfig} */
 
 const path = require("path")
 
-module.exports = {
+module.exports = withPayload({
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
 	},
-}
+})
 
 // const nextConfig = {
 // 	// sassOptions: {
